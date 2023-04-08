f0547547ce7391b85cdba4c7ae9a4468ea993cdb
ddy <58058861+ddy-ddy@users.noreply.github.com>
2023-04-08 20:53:47 +0800

    feat: 新增sql笔记"部门工资前三高的员工"

--- Sql/code/185.部门工资前三高的所有员工.sql
+++ Sql/code/185.部门工资前三高的所有员工.sql
@@ -1,11 +1,29 @@
-
 --
 -- @lc app=leetcode.cn id=185 lang=mysql
 --
 -- [185] 部门工资前三高的所有员工
 --
-
 -- @lc code=start
 # Write your MySQL query statement below
+select
+    Employee,
+    Department,
+    Salary
+from
+    (
+        select
+            e.name as Employee,
+            d.name as Department,
+            e.salary as Salary,
+            dense_rank() over(
+                partition by d.name
+                order by
+                    e.salary desc
+            ) as rnk
+        from
+            Employee e
+            left join Department d on e.DepartmentId = d.Id
+    ) temp
+where
+    rnk <= 3
 -- @lc code=end
-
